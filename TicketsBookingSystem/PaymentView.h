83a2c249dfb18522877f21309e8b9f144ddea74d
jaremakulchyk <jaremakulchyk@gmail.com>
2016-10-02 19:26:09 +0300

    Update PaymentView.h

--- TicketsBookingSystem/PaymentView.h
+++ TicketsBookingSystem/PaymentView.h
@@ -2,9 +2,23 @@
 #include "Views.h"
 class PaymentView : public View
 {
-	RailCarInformation railCar;
+	string creditCard;
+	string dateOfExpire;
+	string CVV;
+	int counter = 0;
+
+	bool creditcard_entered = false;
+	bool dateOfExpire_entered = false;
+	bool cvv_entered = false;
+
+	void enterLine(string&, bool &, int, int&);
 public:
 	PaymentView();
+	void setCreditCard(string);
+	void setDateOfExpire(string);
+	void setCVV(string);
 
 	void draw();
+	
+	View* handle();
 };
